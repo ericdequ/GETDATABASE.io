f8c0576496f0d138236594e4ea84180b11c5def9
ericdequ <ericdequevedo@gmail.com>
2022-07-21 13:08:24 -0400

    GetFile
    File Getter

---
+++ gitfilegetter.js
@@ -0,0 +1,18 @@
+import {octokit, App } from "octokit";
+
+//core.js # readme
+
+/*
+const octokit = new Octokit({
+    auth: ''
+
+})
+*/
+
+await octokit.request('GET /repos/{owner}/{repo}/contents/{path}',{
+    owner: 'EricDequ',
+    repo: 'GetExtension',
+    path: 'GetExtension/Scrapping/BeautifulSoup/CMS/CMSdata.json'
+})
+
+
