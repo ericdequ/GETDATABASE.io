de86fce9a84bcb9aaa41fee67d72a275b513d0c5
ericdequ <ericdequevedo@gmail.com>
2022-07-21 13:32:15 -0400

    look
    look

--- gitfilegetter.js
+++ gitfilegetter.js
@@ -1,12 +1,20 @@
-import { Octokit, App } from "octokit";
+import { Octokit, App } from "@octokit/rest";
 // Octokit.js
 // https://github.com/octokit/core.js#readme
+const fs = require('fs');
+
+
 const octokit = new Octokit({
   auth: 'personal-access-token123'
 });
 
-await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-  owner: 'OWNER',
-  repo: 'REPO',
-  path: 'PATH'
-})
+var dict = {};
+
+dict = octokit.request('GET /repos/{owner}/{repo}/contents/{path}',{
+  owner: 'Ericdequ',
+  repo: 'GetExtension',
+  path: 'Scrapping/BeautifulSoup/CMS/CMSdata.json'
+});
+
+
+console.log(dict[0],dict[1]);
