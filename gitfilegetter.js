8f99a99e74cc8b9023cf6f656bf8d0703d2e624f
ericdequ <ericdequevedo@gmail.com>
2022-07-21 14:10:03 -0400

    latest
    version

--- gitfilegetter.js
+++ gitfilegetter.js
@@ -1,20 +1,47 @@
-import { Octokit, App } from "@octokit/rest";
+const { Octokit } = require("@octokit/rest");
 // Octokit.js
 // https://github.com/octokit/core.js#readme
 const fs = require('fs');
+//const config = require("./config.json");
 
 
+/*
 const octokit = new Octokit({
   auth: 'personal-access-token123'
 });
+*/
+//var dict1 = {};
 
+//dict1 = Octokit.request('Ge')
+
+/* Get JSON from github and show first 2 elements*/
 var dict = {};
-
-dict = octokit.request('GET /repos/{owner}/{repo}/contents/{path}',{
+dict = Octokit.request('GET /repos/{owner}/{repo}/contents/{path}',{
   owner: 'Ericdequ',
   repo: 'GetExtension',
   path: 'Scrapping/BeautifulSoup/CMS/CMSdata.json'
 });
 
 
-console.log(dict[0],dict[1]);
+
+const jsonString = JSON.stringify(dict);
+console.log(jsonString);
+
+
+
+/* Read Json File Locally parse data 
+fs.readFile("./customer.json", "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("Error reading file from disk:", err);
+      return;
+    }
+    try {
+      const customer = JSON.parse(jsonString);
+      console.log("Customer address is:", customer.address); // => "Customer address is: Infinity Loop Drive"
+    } catch (err) {
+      console.log("Error parsing JSON string:", err);
+    }
+  });
+
+
+*/
